6e9534fd8450559c250988b31f6a51461ef35eb1
Tauqeer Ahmad <tauqeer.ahmad2008@gmail.com>
2018-07-11 04:31:41 +0500

    Added user specific galleries

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -1,63 +1,48 @@
 class GalleriesController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_gallery, only: [:show, :edit, :update, :destroy]
 
-  # GET /galleries
-  # GET /galleries.json
   def index
-    @galleries = Gallery.all
+    @galleries = current_user.galleries
   end
 
-  # GET /galleries/1
-  # GET /galleries/1.json
   def show
   end
 
-  # GET /galleries/new
   def new
     @gallery = Gallery.new
   end
 
-  # GET /galleries/1/edit
   def edit
   end
 
-  # POST /galleries
-  # POST /galleries.json
   def create
     @gallery = Gallery.new(gallery_params)
+    @gallery.user = current_user
 
     respond_to do |format|
       if @gallery.save
         format.html { redirect_to @gallery, notice: 'Gallery was successfully created.' }
-        format.json { render :show, status: :created, location: @gallery }
       else
         format.html { render :new }
-        format.json { render json: @gallery.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /galleries/1
-  # PATCH/PUT /galleries/1.json
   def update
     respond_to do |format|
       if @gallery.update(gallery_params)
         format.html { redirect_to @gallery, notice: 'Gallery was successfully updated.' }
-        format.json { render :show, status: :ok, location: @gallery }
       else
         format.html { render :edit }
-        format.json { render json: @gallery.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /galleries/1
-  # DELETE /galleries/1.json
   def destroy
     @gallery.destroy
     respond_to do |format|
       format.html { redirect_to galleries_url, notice: 'Gallery was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
@@ -69,6 +54,6 @@ class GalleriesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def gallery_params
-      params.require(:gallery).permit(:title, :user_id)
+      params.require(:gallery).permit(:title)
     end
 end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,3 +1,7 @@
 class Gallery < ApplicationRecord
   belongs_to :user
+
+  def owner
+    user.name
+  end
 end
