006055903e43f93c7a8ea16426fe952c40c283d9
Tauqeer Ahmad <tauqeer.ahmad2008@gmail.com>
2018-07-13 05:56:46 +0500

    Removed home controller and updated root path to galleries index

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
     end
   end
   devise_for :users
-  root 'home#index'
+  root 'galleries#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
